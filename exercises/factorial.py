e9877c58b7c7831f479de79cc0a3802903d85088
LambdaXymox <lambda.xymox@gmail.com>
2017-01-14 19:10:27 -0600

    Add unit tests for factorial. Fix a bug into factorial code that doesn't check for floating point values

--- exercises/factorial.py
+++ exercises/factorial.py
@@ -5,5 +5,10 @@ import sys
 def factorial(n):
 	if n < 0:
 		raise ValueError('Factorial is undefined for negative integer values. Got {}'.format(n))
-	else:
-	    return functools.reduce(operator.mul, range(2,n+1), 1)
+	
+	try:
+	    result = functools.reduce(operator.mul, range(2,n+1), 1)
+	except:
+		raise ValueError("factorial() only accepts integral values.")
+
+	return result
--- tests/test_fac.py
+++ tests/test_fac.py
@@ -4,7 +4,7 @@ import exercises.factorial as fac
 
 class TestFactorial(unittest.TestCase):
 
-	def test_fac_should_reject_non_positive_integers(self):
+	def test_fac_should_reject_negative_integers(self):
 		"""
 		Test that fac only accepts positive integers for input.
 		"""
@@ -15,6 +15,9 @@ class TestFactorial(unittest.TestCase):
 
 
 	def test_fac_should_accept_positive_integers(self):
+		"""
+		Factorial should accept positive integers.
+		"""
 		test_value = 600
 
 		try:
@@ -24,7 +27,36 @@ class TestFactorial(unittest.TestCase):
 
 
 	def test_fac_should_reject_non_integer_values(self):
+		"""
+		Factorial is not defined for non-integer values.
+		"""
 		test_value = 600.1
 
 		with self.assertRaises(ValueError):
 			fac.factorial(test_value)
+
+
+	def test_fac(self):
+		"""
+		Our factorial function should produce correct results.
+		"""
+		fac_table = \
+			{ 
+			    0 : 1, 
+		        1 : 1, 
+		        2 : 2, 
+		        3 : 6, 
+		        4 : 24, 
+		        5 : 120, 
+		        6 : 720, 
+		        7 : 5040,
+		        8 : 40320, 
+		        9 : 362880, 
+		        10 : 3628800
+		    }
+
+		for i in fac_table.keys():
+			result = fac.factorial(i)
+			correct_result = fac_table[i]
+			print("({},{})".format(i, correct_result))
+			self.assertEqual(result, correct_result)
