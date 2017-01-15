60b9bc5bea4ca31bf83091bc14452051c03a228e
LambdaXymox <lambda.xymox@gmail.com>
2017-01-14 19:11:36 -0600

    Rename files.

--- exercises/fac.py
+++
@@ -1,9 +0,0 @@
-import functools, operator
-import sys
-
-
-def factorial(n):
-	if n < 0:
-		raise ValueError('Factorial is undefined for negative integer values. Got {}'.format(n))
-	else:
-	    return functools.reduce(operator.mul, range(2,n+1), 1)
